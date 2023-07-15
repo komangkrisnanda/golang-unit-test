7aeb12ac0bc300f030c5cdd8b8cc2c0c3ddcbecd
Komang Krisnanda <officialkrisnandha@gmail.com>
2023-07-15 12:05:00 +0800

    update: test Fail and FailNow

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -1,19 +1,26 @@
 package helper
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestHelloWorld(t *testing.T){
 	name := HelloWorld("World")
 
 	if name != "Hello World"{
-		panic("Test failed.")
+		t.Fail()
 	}
+
+	fmt.Println("TestHelloWorld Done")
 }
 
 func TestHelloMichael(t *testing.T){
 	name := HelloWorld("Michael")
 
 	if name != "Hello Michael"{
-		panic("Test failed.")
+		t.FailNow()
 	}
+
+	fmt.Println(("TestHelloMichael Done"))
 }
