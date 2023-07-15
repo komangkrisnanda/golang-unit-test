8acb5d662517b662ff477ba4b9b61e73e44f8bc5
Komang Krisnanda <officialkrisnandha@gmail.com>
2023-07-15 17:44:38 +0800

    impl: benchmark

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -9,6 +9,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func BenchmarkHelloWorld(b *testing.B){
+	for i := 0; i < b.N; i++ {
+		HelloWorld("Taylor Swift")
+	}
+}
+
 func TestMain(m *testing.M){
 	//before
 	fmt.Println("---- UNIT TEST START ----")
