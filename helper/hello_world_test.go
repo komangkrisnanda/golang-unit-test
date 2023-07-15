295dfe663dac899debc92fbbd3f37093eb14e678
Komang Krisnanda <officialkrisnandha@gmail.com>
2023-07-15 11:57:26 +0800

    update: unit test function

--- helper/hello_world_test.go
+++ helper/hello_world_test.go
@@ -3,9 +3,17 @@ package helper
 import "testing"
 
 func TestHelloWorld(t *testing.T){
-	name := HelloWorld("John")
+	name := HelloWorld("World")
 
-	if name != "Hello John"{
+	if name != "Hello World"{
+		panic("Test failed.")
+	}
+}
+
+func TestHelloMichael(t *testing.T){
+	name := HelloWorld("Michael")
+
+	if name != "Hello Michael"{
 		panic("Test failed.")
 	}
 }
